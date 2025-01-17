1f91ceb6eed8d35ed93b1cfbfc1d4b54a9d0f2e1
degrandelucas <lucasdegrande15@gmail.com>
2025-01-17 00:36:17 -0300

    implementando funcao para receber informacoes para calculo do imc direto do HTML

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+let IMC = consultarIMC();
+
+function buscarParametroHTML(tag){
+    document.getElementById(tag).value;
+}
+
+function consultarIMC(parametroPeso, parametroAltura) 
+{
+    let peso = buscarParametroHTML('peso');
+    let altura = buscarParametroHTML('altura');
+    let imc = parametroPeso / (parametroAltura * parametroAltura);
+    return imc;
+}
+
 function exibirTextoNaTela(tag, texto){
     let campoTexto = document.getElementById(tag);
     campoTexto.innerHTML = texto;
 }
 
-function exibirTextoTelaInicial() {
-    exibirTextoNaTela('tituloPagina', 'Calculadora de IMC');
-    exibirTextoNaTela('descricao', 'Adivinhe o número secreto entre 1 e ' + numeroMaximo);
-}
-
-exibirTextoTelaInicial();
+exibirTextoNaTela('tituloPagina', 'Calculadora de IMC');
+exibirTextoNaTela('resposta', 'IMC: ' + IMC);
