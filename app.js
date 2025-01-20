8ce171947b6182133e5d683506f3602871386006
degrandelucas <lucasdegrande15@gmail.com>
2025-01-20 19:15:17 -0300

    finalizando e chamando a funcao destacarLinhaTabela()

--- app.js
+++ app.js
@@ -11,14 +11,13 @@ function calculoIMC()
 {
     let peso = parseFloat(buscarValorHTML('peso').replace(',', '.'));
     let altura = parseFloat(buscarValorHTML('altura').replace(',', '.'));
-    let imc = peso / (altura * altura);
+    let imc = (peso / (altura * altura)).toFixed(1).replace('.', ',');
     return imc;
 }
 
 function informarIMC(){
     let IMC = calculoIMC();
-    let texto = IMC.toFixed(2);
-    exibirTextoNaTela('resultado', texto);
+    exibirTextoNaTela('resultado', IMC);
 }
 
 exibirTextoNaTela('tituloPagina', 'Calculadora de IMC');
@@ -29,7 +28,7 @@ function limparCampos(){
     document.getElementById('resultado').innerHTML = '';
     document.getElementById('menorPesoIdeal').innerHTML = '';
     document.getElementById('maiorPesoIdeal').innerHTML = '';
-
+    removerDestaque();
 }
 
 function menorPesoIdeal(){
@@ -42,4 +41,30 @@ function maiorPesoIdeal(){
     let altura = parseFloat(buscarValorHTML('altura').replace(',', '.'));
     let maiorPeso = (24.9 * (altura * altura)).toFixed(1).replace('.', ',');
     exibirTextoNaTela('maiorPesoIdeal', maiorPeso);
+}
+
+function removerDestaque() {
+    let linhas = document.querySelectorAll('tbody tr');
+    linhas.forEach(linha => {
+        linha.classList.remove('highlight');
+    });
+}
+
+function destacarLinhaTabela() {
+    removerDestaque();
+    let resultadoImc = calculoIMC().replace(',', '.');
+    let linhas = document.querySelectorAll('tbody tr');
+    if (resultadoImc < 18.5) {
+        linhas[0].classList.add('highlight');
+    } else if (resultadoImc >= 18.5 && resultadoImc <= 24.9) {
+        linhas[1].classList.add('highlight');
+    } else if (resultadoImc >= 25 && resultadoImc <= 29.9) {
+        linhas[2].classList.add('highlight');
+    } else if (resultadoImc >= 30 && resultadoImc <= 34.9) {
+        linhas[3].classList.add('highlight');
+    } else if (resultadoImc >= 35 && resultadoImc <= 39.9) {
+        linhas[4].classList.add('highlight');
+    } else {
+        linhas[5].classList.add('highlight');
+    }
 }
